534b8ccc3601025cc1bc8bf6d8c4ffda5c190a86
SumiDuck <duckidoki@gmail.com>
2025-01-10 11:03:43 +0800

    Renamed scripts, created new scripts for view cart page Created new css style for checkout and view cart

--- script/modal-addtocart.js
+++ script/modal-addtocart.js
@@ -43,7 +43,7 @@ dragButton.addEventListener("click", () => {
 
 document.querySelectorAll('.food-card').forEach(card => {
     card.addEventListener('mouseenter', () => {
-        card.style.transform = 'translateY(-10px)';
+        card.style.transform = 'translateY(-2px)';
         card.style.boxShadow = '0 5px 15px rgba(0, 0, 0, 0.2)';
     });
     card.addEventListener('mouseleave', () => {
