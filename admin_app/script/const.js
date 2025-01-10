7c1da461d0f5b4c3b951a9a577ce01547cdbaf8a
samiralatteef@gmail.com <samiralatteef@gmail.com>
2025-01-10 16:44:51 +0800

    updated dropdown nav function

---
+++ admin_app/script/const.js
@@ -0,0 +1,13 @@
+// Dropdown navigation menu titles
+const _ORDERS_TITLE = "order";
+const _DISHES_TITLE = "dishes";
+const _USERS_TITLE = "users";
+const _LOGIN_TITLE = "login";
+const _LOGOUT_TITLE = "logout";
+
+// correspononding site pages' links
+const _ORDERS_URL = "#"; 
+const _DISHES_URL = "dishes.html"; 
+const _USERS_URL = "#"; 
+const _LOGIN_URL = "#"; 
+const _LOGOUT_URL = "#"; 
--- admin_app/script/navController.js
+++ admin_app/script/navController.js
@@ -1,20 +1,39 @@
 /* To create dynamic navigation */
-class NavController{
+class NavController {
+	constructor(element) {
+		this.siteMenu = document.getElementById(element);
+		this.dropdownMenu = document.createElement("ul");
+		this.dropdownItems = [
+			{ title: _ORDERS_TITLE, url: _ORDERS_URL },
+			{ title: _DISHES_TITLE, url: _DISHES_URL },
+			{ title: _USERS_TITLE, url: _USERS_URL },
+			{ title: _LOGIN_TITLE, url: _LOGIN_URL },
+			{ title: _LOGOUT_TITLE, url: _LOGOUT_URL },
+		];
+	}
 
-    constructor(element){
-        this.siteMenu = document.getElementById(element);
-        this.dropdownMenu = document.createElement("ul");
-        this.dropdownItems = [
-            {"title":_ORDERS_TITLE, "url": _ORDERS_URL}, 
-            {"title":_DISHES_TITLE, "url": _DISHES_URL}, 
-            {"title":_USERS_TITLE, "url": _USERS_URL},
-            {"title":_LOGIN_TITLE, "url": _LOGIN_URL}, 
-            {"title":_LOGOUT_TITLE, "url": _LOGOUT_URL},
-        ];
-    }
+	// Function to display dropdown navigation menu dynamically
+	displayNav() {
+		this.dropdownMenu.className = "dropdown-menu text-small shadow";
+		this.siteMenu.appendChild(this.dropdownMenu);
 
-    // Function to display dropdown navigation menu dynamically
-    displayNav(){
+		this.dropdownItems.forEach((item) => {
+			this.displayNavItem(item);
+		});
+	}
 
-    }
+	// Function to display dropdown navigation item dynamically
+	displayNavItem(item) {
+		const dropdownItem = document.createElement("li");
+		this.dropdownMenu, appendChild(dropdownItem);
+		dropdownItem.className = "dropdown-item text-success";
+
+		const dropdownLink = document.createElement("a");
+		dropdownItem.appendChild(dropdownLink);
+		dropdownLink.className = "dropdown-link";
+		dropdownLink.textContent = item.title;
+	}
 }
+
+displayNav();
+displayNavItem(item);
